bfadf3479b47177c7c52427e0b400d8e95e41d8e
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-11 21:06:07 -0500

    add header and add appointment card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-import AddApointments from './components/AddAppointments';
+import 'antd/dist/antd.css';
+import { Card } from 'antd';
+import { PlusCircleFilled } from '@ant-design/icons'
+import AddAppointments from './components/AddAppointments';
 
 function App() {
   return (
     <div className="pet-appt">
-       <AddApointments />
+    <div className="site-card-border-less-wrapper">
+    <Card title="Pet Appointment Scheduler">
+    <Card>
+      <p style={{fontSize: '1.2rem'}}>Add Appointment</p>
+      <PlusCircleFilled style={{color: '#1890ff', fontSize: '30px'}} />
+    </Card>
+    </Card>
+    </div>
+    <AddAppointments />
     </div>
   );
 }
