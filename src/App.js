94168938fcc678f57b0a510bc8e2c3ea83718e85
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-11 19:30:16 -0500

    add appointments add component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AddApointments from './components/AddAppointments';
 
 function App() {
   return (
-    <div className="App">
-       hello
+    <div className="pet-appt">
+       <AddApointments />
     </div>
   );
 }
---
+++ src/components/AddApointments.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddApointments() {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default AddApointments;
