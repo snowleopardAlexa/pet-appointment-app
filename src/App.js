ed42978ccd3db0933048f616f26cde4bef88dabc
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-11 21:57:52 -0500

    create more components, show edit form on click event

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import { Card } from 'antd';
-import { PlusCircleFilled } from '@ant-design/icons'
 import AddAppointments from './components/AddAppointments';
 
 function App() {
+
   return (
     <div className="pet-appt">
     <div className="site-card-border-less-wrapper">
-    <Card title="Pet Appointment Scheduler">
-    <Card>
-      <p style={{fontSize: '1.2rem'}}>Add Appointment</p>
-      <PlusCircleFilled style={{color: '#1890ff', fontSize: '30px'}} />
-    </Card>
+    <Card bordered={true}>
+    <p style={{fontSize: '1.5rem', textAlign: 'center'}}>Add Appointment</p>
+    <AddAppointments />
     </Card>
     </div>
-    <AddAppointments />
     </div>
   );
 }
--- src/components/AddAppointments.jsx
+++ src/components/AddAppointments.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card } from 'antd';
+import { PlusCircleFilled } from '@ant-design/icons';
+import EditAppointments from './EditAppointments';
 
 function AddApointments() {
+
+    const [ showForm, setShowForm ] = useState(false)
+    const onClick = () => setShowForm(true);
+
     return (
         <div>
-            hello
+           <Card>
+             <p style={{fontSize: '1.2rem'}}>Add Appointment</p>
+             <PlusCircleFilled 
+               style={{color: '#1890ff', fontSize: '30px'}} 
+               onClick={onClick}
+             />
+            {showForm ? <EditAppointments /> : null}
+           </Card>
         </div>
     )
 }
